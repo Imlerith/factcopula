8d2cb441f278ac87a4b85320b0ce029aa67014a1
Sergey Nasekin <sergey.nasekin@gmail.com>
2016-06-23 11:07:02 +0200

    new files for gauss-hermite quadrature added

---
+++ FactorCopulaSelfMade/fastgl.cpp
@@ -0,0 +1,9 @@
+//
+//  fastgl.cpp
+//  FactorCopulaSelfMade
+//
+//  Created by Sergey Nasekin on 22/06/16.
+//  Copyright (c) 2016 Sergey Nasekin. All rights reserved.
+//
+
+#include "fastgl.h"
---
+++ FactorCopulaSelfMade/fastgl.h
@@ -0,0 +1,14 @@
+//
+//  fastgl.h
+//  FactorCopulaSelfMade
+//
+//  Created by Sergey Nasekin on 22/06/16.
+//  Copyright (c) 2016 Sergey Nasekin. All rights reserved.
+//
+
+#ifndef __FactorCopulaSelfMade__fastgl__
+#define __FactorCopulaSelfMade__fastgl__
+
+#include <stdio.h>
+
+#endif /* defined(__FactorCopulaSelfMade__fastgl__) */
